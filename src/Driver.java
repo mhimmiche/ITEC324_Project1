2dcbac3d5bdbc3f6aad912fea1c52fe1de1d6263
Mehdi Himmiche <mhimmiche@radford.edu>
2017-10-01 21:51:56 -0400

    More comments!

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,13 @@
+/**
+ * Driver class for the project
+ *
+ * @author Mehdi Himmiche
+ */
 public class Driver {
+    /**
+     * Instantiates the frame
+     * @param args
+     */
     public static void main(String[] args) {
         VendorFrame vf = new VendorFrame();
         vf.vendingFrame();
